030e56cc7a742a53f38a444fac3094fbfd0cf435
Miquel Garcia <miquel.garciafernandez@gmail.com>
2011-02-08 22:15:49 +0100

    Small modification in signal_processing module

--- lib/math/signal_processing.rb
+++ lib/math/signal_processing.rb
@@ -6,28 +6,30 @@ module SignalProcessing
 
 
     #  Computes sliding window of an _array_ using _n_samples_ (which is an optional parameter with
-    #  the default value of 10 samples). The _delay_ parameter (also optional parameter))
+    #  the default value of 10 samples). The _delay_ parameter (also optional parameter) indicates
+    #  if the sliding window has to be computed at the *middle* of the interval, at the *front* or
+    #  at the *back*
     def self.compute_sliding_window( array, opts={})
     
         average = Array.new
        
         # Check the hash containing the options for the method
         options = { :n_samples => 10,
-                    :delay     => 'center'}.merge( opts )
+                    :delay     => 'middle'}.merge( opts )
 
         # Run through the array to compute the middle point
         array.each_with_index do |value,index|
 
             # Computes the from and to indices
-            if options[:delay] then
-                from = index - n_samples
-                to   = index
-            elsif
-                from = index - n_samples
-                to   = index
+            if options[:delay] == 'middle' then
+                from = index - n_samples/2
+                to   = index + n_samples/2
+            elsif options[:delay] == 'front'
+                from = index + n_samples
+                to   = index 
             else
-                from = index - n_samples
-                to   = index
+                from = index - n_samples/2
+                to   = index 
             end
 
         
