366e5eb7b34b4eeeca6253602b8fb6142dcfb178
Miquel Garcia <miquel.garciafernandez@gmail.com>
2011-03-04 08:56:37 +0100

    Added initial fundamental analysis based on fundamental price fit

--- stock_agent.py
+++ stock_agent.py
@@ -7,21 +7,57 @@ sys.path.append( 'lib/finance')
 sys.path.append( 'lib/math' )
 
 import math
+import numpy as np
+import matplotlib as mpl
+import pylab as pl
+import scipy
+from scipy import optimize
+import datetime
+
 
 import yahoo_finance_interface as yfi
-import datetime
 import csv_helper as ch
-import matplotlib as mpl
-import pylab  as pl
+
 import list_extension as le
 import dict_extension as de
 import signal_processing as sp
 
+
+def fit_fundamental_analysis( time, prices ):
+    """ Fundamental analysis found at http://es.wikipedia.org/wiki/An%C3%A1lisis_fundamental """
+
+    # Target function
+    fitfunc = lambda p, x: p[0] + p[1]*np.cos( p[2]*x + p[3] ) 
+    # Distance to the target function
+    errfunc = lambda p, x, y: fitfunc(p, x) - y 
+
+    ## Initial guess for the parameters
+    p0 = [ prices[0], 1.0, 1.0, 0.0 ]
+
+    t = np.array(time)
+    p = np.array(prices)
+
+    p1,success = optimize.leastsq(errfunc, p0, args=(t, p))
+
+    print p1
+
+
+    #pl.plot( time, prices)
+    pl.plot( fitfunc( p1, t) )
+    pl.show()
+
+    return p1
+
+
+
+
+
+
 start_date = datetime.date(2007, 10,10)
 today      = datetime.date.today()
 
-#historical = yfi.fetch_historical_prices( "CRI.MC", start_date, today)
-historical = yfi.fetch_historical_prices( "TEF.MC", start_date, today)
+historical = yfi.fetch_historical_prices( "CRI.MC", start_date, today)
+#historical = yfi.fetch_historical_prices( "TEF.MC", start_date, today)
 
 historical = ch.store_csv( historical, True, ',')
 
@@ -33,10 +69,6 @@ dates = le.reverse( historical['Date'] )
 price_ema_slow = sp.compute_ema( price, 30 )
 price_ema_fast = sp.compute_ema( price, 10 )
 
-#pl.plot( price )
-#pl.plot( price_ema_slow )
-#pl.plot( price_ema_fast )
-#pl.draw()
 
 # Simulation 
 account   = 6000.0
@@ -54,7 +86,13 @@ n_sessions = len( price )
 
 operations_dates = []
 operations = []
+dt = []
 
+start_date =  datetime.datetime.strptime( dates[1], "%Y-%m-%d" )
+
+#pl.plot( price_ema_slow )
+#pl.plot( price_ema_fast )
+#pl.draw()
 for i in xrange( n_sessions ):
 
     if i == 0.0: continue
@@ -65,6 +103,7 @@ for i in xrange( n_sessions ):
                      ( price_ema_fast[i-1] >  price_ema_slow[i-1] ) ) 
     
     date = datetime.datetime.strptime( dates[i], "%Y-%m-%d" )
+    dt.append( (date-start_date).days )
 
     # Check stop loss condition
     if n_stocks > 0:
@@ -85,5 +124,9 @@ for i in xrange( n_sessions ):
         operations_dates.append(date)
         operations.append( account )
 
-pl.plot( operations_dates, operations,'k.-' )
+print len(dt), len(price[1:])
+
+print fit_fundamental_analysis( dt, price[1:] )
+pl.plot( dt, price[1:] )
+#pl.plot( operations_dates, operations,'k.-' )
 pl.show()
